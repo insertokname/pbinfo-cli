11d640fafcfe204bb8ffb5e9f28734426d41150e
insertokername <111150085+insertokername@users.noreply.github.com>
2024-05-06 02:23:03 +0300

    better dependecy management

--- default.nix
+++ default.nix
@@ -1,10 +1,5 @@
-let
-  pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz";
-    # user `nix-prefetch-url --unpack` to ge sha 
-    sha256 = "00rghzfjah557h1f3nynnbxlb3l967p0m1krmqagfs4a9cq39f5m";
-  }) {};
-in
-  pkgs.mkShell {
-    buildInputs = with pkgs; [ cargo openssl pkg-config ];
-  }
+{pkgs?import <nixpkgs>{}}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [ cargo openssl pkg-config ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
